94a96ed17a5ac2a6499a871c9784524960176477
Gianin Basler <gianin.basler@adnovum.ch>
2021-09-15 14:28:52 +0200

    Add error information, refactor
    Add the transaction status and error information in case of a failure to the persisted extrinsic data.
    Refactor the code to make it easier to understand.

--- extrinsic-history/src/mappings/Extrinsic.ts
+++ extrinsic-history/src/mappings/Extrinsic.ts
@@ -1,4 +1,5 @@
 import { SignedBlock, EventRecord, Balance } from '@polkadot/types/interfaces';
+import { GenericExtrinsic } from '@polkadot/types';
 import { Extrinsic } from '../types';
 import { SignedBlockExtended } from '@polkadot/api-derive/type/types';
 
@@ -10,33 +11,49 @@ export async function handleBlock(thisBlock: SignedBlock): Promise<void> {
 			entity.blockNumber = thisBlock.block.header.number.toBigInt();
 			entity.source = extrinsic.signer.toString();
 
-			const [actualArgs] = extractArgs(extrinsic.args)
-			if (actualArgs[0]) {
-				entity.destination = actualArgs[0].toString()
-			}
-			if (actualArgs[1]) {
-				entity.amount = BigInt(actualArgs[1].toString())
-			}
-
-			const totalFee = extractTotalFeeFromEvents(thisBlock as SignedBlockExtended)
-			const tip = extrinsic.tip.toBigInt()
-			entity.totalFee = totalFee
-			entity.tip =  tip
-			entity.fee = totalFee - tip
+			extractAndSetDestination(extrinsic, entity)
+			extractAndSetFees(thisBlock as SignedBlockExtended, extrinsic.tip.toBigInt(), entity)
+			extractAndSetStatus(thisBlock as SignedBlockExtended, entity)
 
 			await entity.save();
 		}
 	}));
 }
 
+function extractAndSetDestination(extrinsic: GenericExtrinsic, entity: Extrinsic) {
+	const [actualArgs] = extractArgs(extrinsic.args)
+	if (actualArgs[0]) {
+		entity.destination = actualArgs[0].toString()
+	}
+	if (actualArgs[1]) {
+		entity.amount = (actualArgs[1] as Balance).toBigInt()
+	}
+}
+
+function extractAndSetFees(blockExtended: SignedBlockExtended, tip: bigint, entity: Extrinsic) {
+	const feeEvent = blockExtended.events.find(event => isFeeEvent(event))
+	const totalFee = (feeEvent.event.data[1] as Balance).toBigInt()
+	entity.totalFee = totalFee
+	entity.tip =  tip
+	entity.fee = totalFee - tip
+}
+
+function extractAndSetStatus(blockExtended: SignedBlockExtended, entity: Extrinsic) {
+	const errorEvent = blockExtended.events.find(event => isErrorEvent(event))
+	if (errorEvent) {
+		entity.status = "Failed"
+		const error = JSON.parse(errorEvent.event.data[0].toString())
+		entity.moduleIndex = error['module'].index
+		entity.errorIndex = error['module'].error
+	}
+	else {
+		entity.status = "Success"
+	}
+}
+
 function extractArgs(args: unknown[]): [unknown[]] {
 	const actualArgs = args.slice();
 	return [actualArgs];
-}
-
-function extractTotalFeeFromEvents(blockExtended: SignedBlockExtended): bigint {
-	const feeEvent = blockExtended.events.find(event => isFeeEvent(event))
-	return (feeEvent.event.data[1] as Balance).toBigInt()
 }
 
 function isTransfer(method: string): boolean {
@@ -45,4 +62,8 @@ function isTransfer(method: string): boolean {
 
 function isFeeEvent(event: EventRecord) {
 	return event.event.index.toHex() == "0x0404";
+}
+
+function isErrorEvent(event: EventRecord) {
+	return event.event.index.toHex() == "0x0001";
 }
